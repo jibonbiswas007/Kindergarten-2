c70331de8ed4c2963d675340350dc701a35ab636
jibonbiswas007 <33145139+jibonbiswas007@users.noreply.github.com>
2018-02-25 21:04:03 +0600

    Add files via upload

---
+++ asset/js/main.js
@@ -0,0 +1,13 @@
+(function($) {
+    "use strict";
+    
+    jQuery(document).ready(function($){
+        var s = skrollr.init();
+    });
+    
+    jQuery(window).load(function(){
+        
+    });
+    
+    
+}(jQuery));
